e3581c732aed271ebec82848591bbdf3c3f5cc57
Ben Chin <bechin@cpp.edu>
2016-10-03 14:41:13 -0700

    Now prints result of calling a java subprocess.

--- ProcessTest.java
+++ ProcessTest.java
@@ -1,7 +1,10 @@
+import java.io.IOException;
+
 public class ProcessTest{
 
-	public static void main(String[] args){
-		ProcessBuilder pb = new ProcessBuilder("java");
+	public static void main(String[] args) throws IOException{
+		ProcessBuilder javaBuilder = new ProcessBuilder("java").inheritIO();
+		Process java = javaBuilder.start();
 	}
 
 }
